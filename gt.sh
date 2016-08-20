c16ed08bb31e6f806be8b4478cefb8e0e34f8af3
Jeremiah Carpenter <edmundjc@gmail.com>
2016-08-20 13:34:06 -0500

    Changed bash script opener for more platform compatibility

--- gt.sh
+++ gt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #                 Get Last number in each line | remove quote  |     math       | remove quote & whitespace   | reorder
 echo 'age,count' && grep -o '[0-9]\+\"*$' "$*" | sed 's/\"//g' | sort | uniq -c | sed 's/^[ \t]*//g;s/\ /,/g' | awk '{print $2,$1}'
