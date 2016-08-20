7b095e29a5efa34db855daeaec3a49613ec76947
Jeremiah Carpenter <edmundjc@gmail.com>
2016-08-20 13:47:57 -0500

    awk wasn't playing nice.  Order is count,age

--- gt.sh
+++ gt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#                 Get Last number in each line | remove quote  |     math       | remove quote & whitespace   | reorder
-echo 'age,count' && grep -o '[0-9]\+\"*$' "$*" | sed 's/\"//g' | sort | uniq -c | sed 's/^[ \t]*//g;s/\ /,/g' | awk '{print $2,$1}'
+#                 Get Last number in each line | remove quote  |     math       | remove quote & whitespace
+echo 'count,age' && grep -o '[0-9]\+\"*$' "$*" | sed 's/\"//g' | sort | uniq -c | sed 's/^[ \t]*//g;s/\ /,/g'
