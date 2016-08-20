7e570b465030b424a11cc77f8ac977a9fe4e8570
Jeremiah Carpenter <edmundjc@gmail.com>
2016-08-20 13:33:18 -0500

    Added single line bash script to do the same thing.

---
+++ gt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#                 Get Last number in each line | remove quote  |     math       | remove quote & whitespace   | reorder
+echo 'age,count' && grep -o '[0-9]\+\"*$' "$*" | sed 's/\"//g' | sort | uniq -c | sed 's/^[ \t]*//g;s/\ /,/g' | awk '{print $2,$1}'
